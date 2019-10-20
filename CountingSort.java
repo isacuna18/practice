fbdcc37ed08cee8399c23f3f9fc61fbadd456a98
isacuna18 <isabel.soteloa@gmail.com>
2019-10-19 17:30:39 -0700

    Add files via upload

---
+++ CountingSort.java
@@ -0,0 +1,47 @@
+/**
+ * Class with 2 ways of doing Counting sort, one naive way and one "better" way
+ * 
+ * @author 	Akhil Batra
+ * @version	1.1 - April 16, 2016
+ * 
+**/
+public class CountingSort {
+    
+    /**
+     * Counting sort on the given int array. Returns a sorted version of the array.
+     *  does not touch original array (non-destructive method)
+     * DISCLAIMER: this method does not always work, find a case where it fails
+     *
+     * @param arr int array that will be sorted
+     * @return the sorted array
+    **/
+    public static int[] naiveCountingSort(int[] arr) {
+        // find max
+        int max = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        // gather all the counts for each value
+        int[] counts = new int[max + 1];
+        for (int i : arr) {
+            counts[i] += 1;
+        }
+
+        // put the value count times into a new array
+        int[] sorted = new int[arr.length];
+        int k = 0;
+        for (int i = 0; i < counts.length; i += 1) {
+            for (int j = 0; j < counts[i]; j += 1, k += 1) {
+                sorted[k] = i;
+            }
+        }
+
+        // return the sorted array
+        return sorted;
+    }
+
+    
+}
